65e2402688526ae38e12dea652bb1afa1ecff36d
Sebastian Kosak <54958619+seb711@users.noreply.github.com>
2023-12-04 11:48:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://paullampe.github.io/saneQI/",
+  // https://vitejs.dev/guide/static-deploy
+  base: "/saneQI/",
 })
